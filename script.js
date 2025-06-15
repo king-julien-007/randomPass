f0832d4c63c7bf52cda6ebfec92cb971611b5a32
king-julien-007 <1anjaan.kallara@gmail.com>
2025-06-15 18:34:54 +0530

    New edit

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 document.addEventListener('DOMContentLoaded', async function () {
-    const response = await fetch('https://api.api-ninjas.com/v1/passwordgenerator?length=16',
+async function getData() {
+        const response = await fetch('https://api.api-ninjas.com/v1/passwordgenerator?length=16',
         {
             method:'GET',
-            headers:{'X-Api-Key': 'BWWW60cexQO3iXDuo1I5Jw==tY5MtYsL8OkuoEmC'},
-            'Content-Type': 'application/json'
+            headers:{'X-Api-Key': 'BWWW60cexQO3iXDuo1I5Jw==tY5MtYsL8OkuoEmC',
+            'Content-Type': 'application/json'}
 
         }
     )
     
     const data = await response.json()
-    document.getElementById('pass').innerHTML = data.password
+    document.getElementById('pass').innerHTML = data.random_password
+}
+
+getData()
+
+document.getElementById('button').addEventListener('click', function(){
+    getData()
+})
 })
